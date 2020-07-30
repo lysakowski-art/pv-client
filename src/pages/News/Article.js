c70bd2c5421625865f4f9e6d98f6de2c5a64021c
lysakowski-art <lysakowski.michal@gmail.com>
2020-07-30 18:06:00 +0200

    update

--- src/pages/News/Article.js
+++ src/pages/News/Article.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactHtmlParser from "react-html-parser";
-import isOdd from "is-odd"
+import isOdd from "is-odd";
 
-const Article = ({ article,index }) => {
+const Article = ({ article, index }) => {
+  const [classToggle, setClassToggle] = useState(true);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setClassToggle(!classToggle);
+  };
+
   if (isOdd(index)) {
     return (
       <li key={article.index}>
         <div className="article-cnt ">
-          <div className="article-content">
+          <div
+            className={
+              classToggle
+                ? "article-content"
+                : "article-content article-content-active"
+            }
+          >
             <h4 className="article-title">{article.name}</h4>
             <p className="article-text">{ReactHtmlParser(article.content)}</p>
-            <button className="article-button">Więcej</button>
+            <button onClick={handleClick} className="article-button">
+              {classToggle ? "Więcej" : "Mniej"}
+            </button>
           </div>
           <img
             className="article-img"
@@ -19,36 +34,35 @@ const Article = ({ article,index }) => {
           />
         </div>
       </li>
-    )} else {
-     return (<li key={article.index}>
-      <div className="article-cnt article-cnt-oposite">
-        <div className="article-content">
-          <h4 className="article-title">{article.name}</h4>
-          <p className="article-text">{ReactHtmlParser(article.content)}</p>
-          <button className="article-button oposite-button">Więcej</button>
+    );
+  } else {
+    return (
+      <li key={article.index}>
+        <div className="article-cnt article-cnt-oposite">
+          <div className={
+            classToggle
+            ? "article-content"
+            : "article-content  article-content-active"
+          
+          }>
+            <h4 className="article-title">{article.name}</h4>
+            <p className="article-text">{ReactHtmlParser(article.content)}</p>
+            <button
+              onClick={handleClick}
+              className="article-button oposite-button"
+            >
+              {classToggle ? "Więcej" : "Mniej"}
+            </button>
+          </div>
+          <img
+            className="article-img"
+            src="https://www.placecage.com/c/300/300"
+            alt="jakis cage"
+          />
         </div>
-        <img
-          className="article-img"
-          src="https://www.placecage.com/c/300/300"
-          alt="jakis cage"
-        />
-      </div>
-    </li>)
+      </li>
+    );
   }
 };
 
 export default Article;
-
-{/* <li key={article.index}>
-      <div className="article-cnt">
-        <div className="article-content">
-          <h4 className="article-title">{article.name}</h4>
-          <p className="article-text">{ReactHtmlParser(article.content)}</p>
-        </div>
-        <img
-          className="article-img"
-          src="https://www.placecage.com/c/300/300"
-          alt="jakis cage"
-        />
-      </div>
-    </li> */}
--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -6,21 +6,24 @@ import Article from "./Article";
 const News = () => {
   return (
     <div className="news-main">
-      <Query query={getNews}>
-        {({ loading, error, data }) => {
-          if (loading) return "Loading...";
-          if (error) return `Error: ${error.message}`;
-          const { news } = data;
-          const articles = news.map((article, index) => (
-            <Article key={index} article={article} index={index} />
-          ));
-          return (
-            <div className="news-cnt">
-              <ul>{articles}</ul>
-            </div>
-          );
-        }}
-      </Query>
+ 
+      <div className="news-wrapper">
+        <Query query={getNews}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error: ${error.message}`;
+            const { news } = data;
+            const articles = news.map((article, index) => (
+              <Article key={index} article={article} index={index} />
+            ));
+            return (
+              <div className="news-cnt">
+                <ul>{articles}</ul>
+              </div>
+            );
+          }}
+        </Query>
+      </div>
     </div>
   );
 };
