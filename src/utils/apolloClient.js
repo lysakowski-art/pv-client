cf91a1227a42cd56e3848b0213b8668a18b7095a
Ubuntu <ubuntu@ip-172-31-45-89.eu-west-2.compute.internal>
2020-08-07 12:02:50 +0000

    update

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -4,8 +4,17 @@ import { HttpLink } from "apollo-link-http";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
-  // uri: `http://localhost:1337/graphql`
+  //uri: `${process.env.REACT_APP_BACKEND_URL}/graphql`,
+uri: "http://3.10.35.206:1337/graphql",
+//credentials: 'include',
+//  fetchOptions: {
+//    mode: 'no-cors',
+//  },
+//headers: {
+//  "Content-Type": "application/graphql",
+//  "Accept": "*/*",
+//  "Accept-Encoding": "gzip, deflate, br"
+//}
 });
 
 const client = new ApolloClient({
